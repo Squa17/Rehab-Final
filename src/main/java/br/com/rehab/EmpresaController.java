da0180b100d674ed0f4182d732ab08960bfcf941
FelipeSysten <76599815+FelipeSysten@users.noreply.github.com>
2023-11-22 00:35:35 -0300

    deploy7

---
+++ src/main/java/br/com/rehab/EmpresaController.java
@@ -0,0 +1,73 @@
+package br.com.rehab;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import br.com.rehab.model.Empresa;
+import br.com.rehab.services.EmpresaService;
+
+
+
+@Controller
+@RequestMapping("/empresas")
+public class EmpresaController {
+
+	@Autowired
+	private EmpresaService empresaService;
+
+	// Listar
+		@GetMapping
+		public String listEmpresas(Model model) {
+			List<Empresa> empresas = empresaService.getAllEmpresas();
+			model.addAttribute("empresas", empresas);
+			return "ListarEmpresas";
+		}
+
+	// Formulário de criação
+	@GetMapping("/novo")
+	public String showFormForAdd(Model model) {
+		Empresa empresa = new Empresa();
+		model.addAttribute("empresa", empresa);
+		return "empresaForm";
+	}
+
+	// Persistencia da criação
+	@PostMapping("/save")
+	public String saveEmpresa(@ModelAttribute("empresa") Empresa empresa) {
+		empresaService.saveEmpresa(empresa);
+		return "redirect:/empresas";
+	}
+
+	// Formulário de edição
+	@GetMapping("/editar/{id}")
+	public String showFormForUpdate(@PathVariable Long id, Model model) {
+		Empresa empresa = empresaService.getEmpresaById(id);
+		model.addAttribute("empresa", empresa);
+		return "editarEmpresa";
+	}
+
+	// Persistencia da edição
+	@PostMapping("/editar/{id}")
+	public String updateEmpresa(@PathVariable Long id, @ModelAttribute("empresa") 
+	Empresa empresa) {
+		empresaService.updateEmpresa(id, empresa);
+		return "redirect:/empresas";
+	}
+	
+	// Excluir categoria
+	@GetMapping("/deletar/{id}")
+	public String deleteEmpresa(@PathVariable Long id) { 
+		empresaService.deleteEmpresa(id);
+		return "redirect:/empresas";
+	}
+
+}
